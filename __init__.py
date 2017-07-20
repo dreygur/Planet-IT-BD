7c41d2815d19fc2e13b512251ce97321c1d6b428
Totul <rytotul@gmail.com>
2017-07-21 02:33:27 +0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Copyright (c) 2017 ISTT_Cyborgs (fb.com/groups/istt.cse.2017)
-Author: Rakibul Yeasin Totul (fb.com/rytotul)
-See the file 'doc/COPYING' for copying permission
-"""
-
-pass
