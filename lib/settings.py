d9baf1aca03b184ce4195c5f2d4f9d5014a6d53e
Totul <rytotul@gmail.com>
2017-07-22 01:18:05 +0600

    Create settings.py

---
+++ lib/settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#############################################################################
+#    PLanet IT API - Python 3.6 Planet IT API.
+#    Copyright (C) 2017 by Rakibul Yeasin Totul
+#    URI - https://www.rytotul.xyz/about
+#	 Facebook - https://www.facebook.com/rytotul
+#	 Github - https://www.github.com/rytotul
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+banner = """
+			Planet IT SMS Sending module
+			For help: -h or --help
+			Bug report: rytotul@gmail.com
+		 """
+
+print(banner)
